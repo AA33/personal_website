0ed7e25fb2fcb1cdcf7c701dd826718871fabc69
Abhishek Anurag <anurag.abhishek3@gmail.com>
2014-07-11 21:53:47 -0500

    Copy from heroku sample

--- server/server.js
+++ server/server.js
@@ -21,17 +21,16 @@
  * THE SOFTWARE.
  */
 
-var debug = require('debug')('server');
-var app = require('app');
+var express = require('express')
+var app = express();
 
-app.set('port', process.env.PORT || 5000);
-
+app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
     response.send('Hello World!')
 })
 
-var server = app.listen(app.get('port'), function() {
-    debug('Express server listening on port ' + server.address().port);
-});
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+})
