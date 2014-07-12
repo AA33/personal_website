9d9a830ffae82b396c22d1c261915c972daf56e1
Abhishek Anurag <anurag.abhishek3@gmail.com>
2014-07-12 11:39:39 -0500

    Final post commit hook test.

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-    response.send('Hello World! How are you doing?')
+    response.send('Hello World! How are you doing today?')
 })
 
 app.listen(app.get('port'), function() {
